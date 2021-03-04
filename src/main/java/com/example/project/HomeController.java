770d90fefee6c9e0f7c0f97b07c308f2fb628904
mcnc <mcnc@DESKTOP-28V4H9K>
2021-03-04 11:26:02 +0900

    dfhdgh

--- src/main/java/com/example/project/HomeController.java
+++ src/main/java/com/example/project/HomeController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	String name = "hello2";
+	String name = "helulo2";
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
